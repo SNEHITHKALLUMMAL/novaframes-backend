2b53095c7d7735d7f3b09ad3f35c21c50c268628
SNEHITHKALLUMMAL <snehithkallummal999@gmail.com>
2026-08-30 11:57:44 +0530

    Delete server directory

--- server/src/controllers/payment.controller.js
+++
@@ -1,8 +0,0 @@
-import { asyncHandler } from '../utils/asyncHandler.js';
-import { sendSuccess } from '../utils/ApiResponse.js';
-import * as subscriptionService from '../services/subscription.service.js';
-
-export const listMyPayments = asyncHandler(async (req, res) => {
-  const payments = await subscriptionService.listMyPayments(req.user._id);
-  sendSuccess(res, { message: 'Payment history retrieved', data: { payments } });
-});
