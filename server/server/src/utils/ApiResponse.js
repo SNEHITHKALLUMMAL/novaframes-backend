987795711c7c59cf257ea724fc5b336f65d4e6b6
SNEHITHKALLUMMAL <snehithkallummal999@gmail.com>
2026-08-31 19:37:35 +0530

    Delete server directory

--- server/server/src/utils/ApiResponse.js
+++
@@ -1,7 +0,0 @@
-/**
- * Standard success envelope, matching the SRS api_architecture.response_format:
- * { success: true, message, data }
- */
-export function sendSuccess(res, { statusCode = 200, message = 'Success', data = {} } = {}) {
-  return res.status(statusCode).json({ success: true, message, data });
-}
