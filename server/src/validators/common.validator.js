4b2619afac252e8432e15b1dd29f1339d960bb46
SNEHITHKALLUMMAL <snehithkallummal999@gmail.com>
2026-09-04 11:34:37 +0530

    Delete server directory

--- server/src/validators/common.validator.js
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-const OBJECT_ID_RE = /^[0-9a-fA-F]{24}$/;
-
-export const objectIdSchema = z.string().regex(OBJECT_ID_RE, 'Invalid id format');
-
-export const idParamSchema = z.object({
-  id: objectIdSchema,
-});
--- server/src/validators/model.validator.js
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-import { GENERATION_TYPES } from '../constants/enums.js';
-
-export const listModelsQuerySchema = z.object({
-  type: z.enum(Object.values(GENERATION_TYPES)).optional(),
-});
