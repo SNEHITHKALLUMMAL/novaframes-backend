ca900579aaef39be6e191912987da8c018f3a110
SNEHITHKALLUMMAL <snehithkallummal999@gmail.com>
2026-09-04 20:44:36 +0530

    Delete server directory

--- server/src/controllers/dashboard.controller.js
+++
@@ -1,8 +0,0 @@
-import { asyncHandler } from '../utils/asyncHandler.js';
-import { sendSuccess } from '../utils/ApiResponse.js';
-import * as dashboardService from '../services/dashboard.service.js';
-
-export const getSummary = asyncHandler(async (req, res) => {
-  const summary = await dashboardService.getDashboardSummary(req.user._id);
-  sendSuccess(res, { message: 'Dashboard summary retrieved', data: summary });
-});
