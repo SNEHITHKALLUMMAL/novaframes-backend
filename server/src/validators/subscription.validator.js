f1fce5a979e4cbdb65b0720c5bb6752cbacf04b4
SNEHITHKALLUMMAL <snehithkallummal999@gmail.com>
2026-09-04 12:49:51 +0530

    Delete server directory

--- server/src/validators/subscription.validator.js
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-import { SUBSCRIPTION_PLANS } from '../constants/enums.js';
-
-export const subscribeSchema = z.object({
-  plan: z.enum(Object.values(SUBSCRIPTION_PLANS)),
-  billingCycle: z.enum(['monthly', 'yearly']).default('monthly'),
-});
